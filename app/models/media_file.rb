cfbfdbc4a61563c1279722a88e0c6a751f3b2711
Patricio Mac Adden <patriciomacadden@gmail.com>
2011-04-15 16:56:37 -0300

    added media_file_id to activity model.

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -11,6 +11,6 @@ class MediaFile < ActiveRecord::Base
   private
   
   def create_media_file_created_activity
-    MediaFileCreatedActivity.create!(:user_a_id => self.user.id, :point_of_interest_id => self.point_of_interest.id)
+    MediaFileCreatedActivity.create!(:user_a_id => self.user.id, :point_of_interest_id => self.point_of_interest.id, :media_file_id => self.id)
   end
 end
