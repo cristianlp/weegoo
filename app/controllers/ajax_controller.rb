dc31d4964059ac75de8c5820f0d4efa1c69a9895
Patricio Mac Adden <patriciomacadden@gmail.com>
2011-05-22 18:59:22 -0300

    added a lot of i18n strings and views improvements

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,6 +1,6 @@
 class AjaxController < ApplicationController
   def update_sub_categories
-    sub_categories = SubCategory.where(:category_id => params[:category_id])
+    sub_categories = SubCategory.where(:category_id => params[:category_id]).order("name")
     
     options = sub_categories.collect { |x| "{\"id\": \"#{x.id}\", \"name\": \"#{x.name}\"}" }
     
