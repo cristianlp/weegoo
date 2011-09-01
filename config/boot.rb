7465b422d334e97a8c80ce1529697968a4630cba
Patricio Mac Adden <patriciomacadden@gmail.com>
2011-09-01 17:36:46 -0300

    fixed the Pysch::SyntaxError thing

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,7 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# fixing Psych::SyntaxError issue
+require 'yaml'
+YAML::ENGINE.yamler = 'syck'
